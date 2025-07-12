d8cd5fa82f234dd3c1efb5ebf476e2f1edf1bbd6
felix0910 <felix@delco.tv>
2025-07-12 09:00:45 -0400

    Overhaul
    website overhaul

--- code.js
+++ code.js
@@ -1,14 +1,31 @@
+const pages = [];
+pages.push("if_home", "if_tutorials", "if_movies", "if_homelab", "if_about", "if_axolotls");
+//page load
+var pageTracker = 0;
+
 window.onload=function(){
-    //page load
+
     mainFrame = document.getElementById('homeframe');
     //activeFrame = homeFrame;
 
-    document.getElementById('movielink').addEventListener('click', function() {
-        mainFrame.src = "movies.html";
+    document.getElementById('if_movies').addEventListener('click', function() {
+        mainFrame.src = "./pages/movies/movies.html"; navLock(2);
     }); 
     
-    document.getElementById('homelink').addEventListener('click', function() {
-        mainFrame.src = "mainpage.html";
+    document.getElementById('if_home').addEventListener('click', function() {
+        mainFrame.src = "./pages/home/home.html"; navLock(0);
+    }); 
+
+    document.getElementById('if_axolotls').addEventListener('click', function() {
+        mainFrame.src = "./pages/axolotls/axolotls.html"; navLock(5);
+    }); 
+
+    document.getElementById('if_about').addEventListener('click', function() {
+        mainFrame.src = "./pages/about/about.html"; navLock(4);
+    }); 
+
+    document.getElementById('if_homelab').addEventListener('click', function() {
+        mainFrame.src = "./pages/homelab/homelab.html"; navLock(3);
     }); 
 };
 
@@ -18,3 +35,13 @@ function play() {
     sound.play();
 }
 
+function navLock(next) {
+    var activenav = document.getElementById(pages[pageTracker]);
+    activenav.classList.add('buttons');
+    activenav.classList.remove('navActive');
+    var newNav = document.getElementById(pages[next]);
+    newNav.classList.add('navActive');
+    newNav.classList.remove('buttons');
+    pageTracker = next;
+    console.log(newNav.outerHTML);
+}
